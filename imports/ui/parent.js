04bcc213e184f44f2cc42fe7a794b1cbca558186
Siva Tallapareddy <stallap@deakin.edu.au>
2018-09-02 22:22:52 +1000

    Paren dashboard
    TestSync

---
+++ imports/ui/parent.js
@@ -0,0 +1,7 @@
+import { Template } from 'meteor/templating';
+import { Tasks } from '../api/task.js';
+import { Tasks1 } from '../api/task.js';
+
+import './parent.html';
+import './feed.js';
+import './top.html';
