2b530e1e6e41db8c66ae0df451f019282ed4ddd0
Bishal Shrestha <bishal.revenger@gmail.com>
2018-09-09 17:58:05 +1000

    Add Facilitator & Display Facilitators

--- imports/ui/LGA/addFacilitators.js
+++ imports/ui/LGA/addFacilitators.js
@@ -1,3 +1,39 @@
 import './addFacilitators.html';
 import '../Others/feed.js';
 import '../Others/routes.js';
+import { Template } from 'meteor/templating';
+import { Facilitators } from '../../api/task';
+
+  Template.addFacilitators.helpers({
+    facilitatorData() {
+        return Facilitators.find({}, { sort: { createdAt: -1 } });
+    },
+  });
+
+  Template.addFacilitators.events({
+    'submit .new-task'(event) {
+      // Prevent default browser form submit
+      event.preventDefault();
+   
+      // Get value from form element
+      const target = event.target;
+      const text = target.text.value;
+   
+      // Insert a task into the collection
+      Facilitators.insert({
+        text,
+        createdAt: new Date(), // current time
+      });
+   
+      // Clear form
+      target.text.value = '';
+    },
+  });
+  // Template.addFacilitators.helpers({
+//     facilitatorData: [
+//       { text: 'This is task 1' },
+//       { text: 'This is task 2' },
+//       { text: 'This is task 3' },
+//     ],
+//   });
+
