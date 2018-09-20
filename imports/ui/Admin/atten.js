c5bad03a9a3772941150728c7142e98d5c3b10c0
SaiSiddeshwar <sska@deakin.edu.au>
2018-09-21 01:47:05 +1000

    New: Login Page

--- imports/ui/Admin/atten.js
+++ imports/ui/Admin/atten.js
@@ -1,15 +1,14 @@
 
-import './atten.html';
-import './admin.js';
-import '../Others/routes.js';
-
 import { Template } from 'meteor/templating';
 import { Tasks1 } from '../../api/task.js';
 import { ReactiveDict } from 'meteor/reactive-dict';
 
+import './atten.html';
+import './admin.js';
+import '../Others/routes.js';
 import '../Others/body.js';
 import '../Others/feed.js';
-import './addGroup.js';
+//import './addGroup.js';
 import { Sessions } from '../../api/task.js';
 
 
@@ -42,7 +41,7 @@ Template.atten.events({
     },
 });
 
-Template.task.events({
+Template.tas.events({
     'click .toggle-checked': function(event){
         Sessions.update(this._id,{
             $set: { checked: ! this.checked },
